fd185625d41b19b126d1381e9d32999d62bcdf69
Aman Gupta <eagleax7@gmail.com>
2024-06-19 22:20:59 +0530

    contact added

--- src/components/Mail.tsx
+++ src/components/Mail.tsx
@@ -26,7 +26,7 @@ const Contact: React.FC = () => {
   });
 const [loading ,setLoading] = useState(false)
  
-  const emailLink = 'https://formspree.io/f/xrgnwlok'
+
   const onSubmit = async(data: ContactFormInputs) => {
   
     setLoading(true);
